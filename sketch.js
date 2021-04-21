42f0cbf0b1ae89e222ea8486ec19dfcbe43db770
city535353 <city535353@gmail.com>
2021-04-21 21:07:49 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://city535353.github.io/imageai/';
+let imageModelURL = 'https://city535353.github.io/imageai2/';
 
 // Video
 let video;
